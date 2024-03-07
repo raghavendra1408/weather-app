a60086d944c5ef0fbc5f57f45fa9d32d6b4fb1fc
raghavendra1408 <raghvendrajadeja22@gmail.com>
2024-03-07 12:17:43 +0530

    weather-app

---
+++ script.js
@@ -0,0 +1,34 @@
+const apiKey = "10e7a3fc418c59b5928763d9dcd0eeba"
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector("#search-btn");
+const weatherIcon = document.querySelector(".weather-icon")
+
+async function checkWeather(city){
+    const response = await fetch(apiURL + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data.weather[0].main);
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "&nbsp; km/hr";
+
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "images/clouds.png"
+    }else if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "images/rain.png"
+    }else if(data.weather[0].main == "Snow"){
+        weatherIcon.src = "images/snow.png"
+    }else if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "images/clear.png"
+    }else if(data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "images/drizzle.png"
+    }else if(data.weather[0].main == "Mist"){
+        weatherIcon.src = "images/mist.png"
+    }
+}
+
+searchBtn.addEventListener("click", () =>{
+    checkWeather(searchBox.value);
+})
